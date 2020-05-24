8b20f0212177823d51b1b81ecff88d9824eb837e
wrist <stoicheia1986@gmail.com>
2020-05-24 17:12:34 +0900

    Add invoke to develop depencency. Add tasks.py used by invoke and removed Makefile

---
+++ tasks.py
@@ -0,0 +1,60 @@
+from invoke import task
+
+import os
+import shutil
+
+@task
+def pytest(c):
+    """execute pytest"""
+    c.run("poetry run pytest")
+
+@task
+def cuitest(c):
+    """execute cui script test"""
+    print("image extract test")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img")
+    print(os.listdir("test/img"))
+    shutil.rmtree("test/img")
+
+    print("audio extract test")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -w test/wav")
+    print(os.listdir("test/wav"))
+    shutil.rmtree("test/wav")
+
+    print("both extract test")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img -w test/wav")
+    print(os.listdir("test/img"))
+    print(os.listdir("test/wav"))
+    shutil.rmtree("test/wav")
+    shutil.rmtree("test/img")
+
+    print("both extract and convert ipynb test")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img -w test/wav -o test/test.converted.ipynb --img-prefix img --wav-prefix wav")
+
+@task
+def format(c):
+    """format nbmediasplit.py"""
+    c.run("poetry run autopep8 -a -i src/nbmediasplit/nbmediasplit.py")
+
+@task
+def clean(c):
+    """clean generated files"""
+    print("remove test/wav, test/img, test/test.converted.ipynb if exists")
+    if os.path.exists("test/wav"):
+        shutil.rmtree("test/wav")
+    if os.path.exists("test/img"):
+        shutil.rmtree("test/img")
+    if os.path.exists("test/test.converted.ipynb"):
+        os.remove("test/test.converted.ipynb")
+
+@task
+def deploy(c):
+    """deplay to pypi"""
+    c.run("poetry build")
+    c.run("poetry publish")
+
+@task
+def deploytest(c):
+    """deplay to testpypi"""
+    c.run("poetry build")
+    c.run("poetry publish -r testpypi")
