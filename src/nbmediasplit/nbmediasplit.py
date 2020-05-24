dde69bfc756e98e276df2b22d4b4d16cbe7520bf
wrist <stoicheia1986@gmail.com>
2020-05-24 17:58:36 +0900

    Add debug option -d and --debug to output logging.debug. Modified cuitest in tasks.py to use debug flag by `inv cuitest -d`

--- src/nbmediasplit/nbmediasplit.py
+++ src/nbmediasplit/nbmediasplit.py
@@ -11,8 +11,6 @@ from pprint import pprint as pp
 import bs4
 import click
 
-# logging.basicConfig(level=logging.DEBUG)
-
 
 def mkdir_p(target_dir):
     """mkdir recursively if not exists"""
@@ -160,10 +158,15 @@ class NBMediaSplitter:
 @click.option('-w', '--wavdir', 'wav_out_dir', type=str, help='directory to store audio', required=False)
 @click.option('-o', '--output', 'new_ipynb_filename', type=str, help='output ipynb file path', required=False)
 @click.option('-e', '--encoding', 'encoding', type=str, help='input ipynb encoding', required=False)
+@click.option('-d', '--debug', 'use_debug', is_flag=True, help='use debug mode', required=False)
 @click.option('--img-prefix', 'img_prefix', type=str, help='path prefix for src attribute of img tag', required=False)
 @click.option('--wav-prefix', 'wav_prefix', type=str,
               help='path prefix for src attribute of source tag under audio tag', required=False)
-def main(ipynb_file, img_out_dir=None, wav_out_dir=None, new_ipynb_filename=None, img_prefix=None, wav_prefix=None, encoding="utf-8"):
+def main(ipynb_file, img_out_dir=None, wav_out_dir=None, new_ipynb_filename=None,
+         img_prefix=None, wav_prefix=None, encoding="utf-8", use_debug=False):
+    if use_debug:
+        logging.basicConfig(level=logging.DEBUG)
+
     splitter = NBMediaSplitter(ipynb_file)
 
     splitter.set_encoding(encoding)
--- tasks.py
+++ tasks.py
@@ -9,27 +9,28 @@ def pytest(c):
     c.run("poetry run pytest")
 
 @task
-def cuitest(c):
+def cuitest(c, debug=False):
     """execute cui script test"""
+    debug_arg = "-d" if debug else ""
     print("image extract test")
-    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img {0}".format(debug_arg))
     print(os.listdir("test/img"))
     shutil.rmtree("test/img")
 
     print("audio extract test")
-    c.run("poetry run nbmediasplit -n test/test.ipynb -w test/wav")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -w test/wav {0}".format(debug_arg))
     print(os.listdir("test/wav"))
     shutil.rmtree("test/wav")
 
     print("both extract test")
-    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img -w test/wav")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img -w test/wav {0}".format(debug_arg))
     print(os.listdir("test/img"))
     print(os.listdir("test/wav"))
     shutil.rmtree("test/wav")
     shutil.rmtree("test/img")
 
     print("both extract and convert ipynb test")
-    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img -w test/wav -o test/test.converted.ipynb --img-prefix img --wav-prefix wav")
+    c.run("poetry run nbmediasplit -n test/test.ipynb -i test/img -w test/wav -o test/test.converted.ipynb --img-prefix img --wav-prefix wav {0}".format(debug_arg))
 
 @task
 def format(c):
